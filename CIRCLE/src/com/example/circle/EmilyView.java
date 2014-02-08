09ae76bbe8468604bddcbf9ee7bd8cda5d6aece3
Emily <Emily@Emily-THINK>
2014-02-08 17:49:27 -0600

    SQUARES!!!11!!1

--- CIRCLE/src/com/example/circle/EmilyView.java
+++ CIRCLE/src/com/example/circle/EmilyView.java
@@ -1,17 +1,25 @@
 package com.example.circle;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class EmilyView extends SurfaceView implements Runnable {
+public class EmilyView extends SurfaceView implements Runnable,OnTouchListener {
 
 	Thread thread = null;
 	SurfaceHolder surfaceHolder;
 	volatile boolean running = false;
+	
+	ArrayList<Square> squares = new ArrayList<Square>();
 	
 	public EmilyView(Context context) {
 		super(context);
@@ -30,6 +38,7 @@ public class EmilyView extends SurfaceView implements Runnable {
 
 	private void init() {
 		surfaceHolder = getHolder();
+		this.setOnTouchListener(this);
 	};
 
 
@@ -63,12 +72,25 @@ public class EmilyView extends SurfaceView implements Runnable {
 		}
 	}
 	
-	int x=0;
-	int y=0;
+
 	
 	private void myDraw(Canvas canvas) {
-		Paint paint = new Paint();
-		paint.setColor(0xff000000+x*0x100);
-		canvas.drawRect(x++, y++, x+100, y+100, paint);
+		Log.i("what is the size of squares?",squares.size()+"");
+		for (int i=0;i<squares.size();i++){
+			Log.i("i",""+i);
+			squares.get(i).draw(canvas);
+		}
+		
+		
+	}
+
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_UP){
+			Paint paint = new Paint();
+			paint.setColor((int)(0xff000000+Math.random()*0x00ffffff));
+			squares.add(new Square(event.getX()-10,event.getX()+10,event.getY()-10,event.getY()+10,paint));
+		}
+		return true;
 	}
 }
---
+++ CIRCLE/src/com/example/circle/Square.java
@@ -0,0 +1,27 @@
+package com.example.circle;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Square {
+	float left;
+	float right;
+	float top;
+	float bottom;
+	Paint paint;
+	
+	public Square(float left, float right, float top, float bottom, Paint paint) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.top = top;
+		this.bottom = bottom;
+		this.paint = paint;
+	}
+
+	
+	public void draw(Canvas canvas) {
+		canvas.drawRect(left, top, right, bottom, paint);
+	}
+
+}
