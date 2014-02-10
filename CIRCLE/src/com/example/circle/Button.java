13317d31510eb0b9795771306dd71752afed969d
Emily <Emily@192.168.1.102>
2014-02-10 17:23:02 -0600

    added text to buttons

---
+++ CIRCLE/src/com/example/circle/Button.java
@@ -0,0 +1,35 @@
+package com.example.circle;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.Log;
+
+public class Button {
+	float left;
+	float right;
+	float top;
+	float bottom;
+	Paint bgpaint;
+	String text;
+	Paint txtpaint;
+	
+	public Button(float left, float right, float top, float bottom, Paint bgpaint, String text, Paint txtpaint) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.top = top;
+		this.bottom = bottom;
+		this.bgpaint = bgpaint;
+		this.text = text;
+		this.txtpaint = txtpaint;
+	}
+
+	
+	public void draw(Canvas canvas) {
+		canvas.drawRect(left, top, right, bottom, bgpaint);
+		Log.i("I drew!", left+","+ top+","+ right+","+ bottom);
+		canvas.drawText(text, left+(right-left)/2, top+(bottom-top)/2, txtpaint);
+		Log.i("I wrote!", text);
+	}
+
+}
--- CIRCLE/src/com/example/circle/EmilyView.java
+++ CIRCLE/src/com/example/circle/EmilyView.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.PorterDuff.Mode;
+import android.graphics.Paint.Align;
+import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.WindowManager;
 
 public class EmilyView extends SurfaceView implements Runnable,OnTouchListener {
 
@@ -20,26 +23,90 @@ public class EmilyView extends SurfaceView implements Runnable,OnTouchListener {
 	SurfaceHolder surfaceHolder;
 	volatile boolean running = false;
 	
-	ArrayList<Square> squares = new ArrayList<Square>();
+	ArrayList<Button> buttons = new ArrayList<Button>();	//list of our buttons
 	
 	public EmilyView(Context context) {
 		super(context);
-		init();
+		init(context);
 	}
 
 	public EmilyView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init();
+		init(context);
 	}
 
 	public EmilyView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init();
+		init(context);
 	}
 
-	private void init() {
+	private void init(Context context) {
 		surfaceHolder = getHolder();
 		this.setOnTouchListener(this);
+		
+		/* modified from Elliott Hughes' "Dalvik Explorer" app
+		 * on http://stackoverflow.com/questions/1016896/how-to-get-screen-dimensions
+		 * 
+		WindowManager w = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Display d = w.getDefaultDisplay();
+		DisplayMetrics metrics = new DisplayMetrics();
+		d.getMetrics(metrics);
+		// since SDK_INT = 1;
+		int width = metrics.widthPixels;
+		int height = metrics.heightPixels;
+		// includes window decorations (statusbar bar/menu bar)
+		if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 17)
+		try {
+		    width = (Integer) Display.class.getMethod("getRawWidth").invoke(d);
+		    height = (Integer) Display.class.getMethod("getRawHeight").invoke(d);
+		} catch (Exception ignored) {
+		}
+		// includes window decorations (statusbar bar/menu bar)
+		if (Build.VERSION.SDK_INT >= 17)
+		try {
+		    Point realSize = new Point();
+		    Display.class.getMethod("getRealSize", Point.class).invoke(d, realSize);
+		    width = realSize.x;
+		    height = realSize.y;
+		} catch (Exception ignored) {
+		}
+		*/
+		
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		
+		DisplayMetrics metrics = new DisplayMetrics();
+		wm.getDefaultDisplay().getMetrics(metrics);
+
+		int height = metrics.heightPixels;	//get screen height (dependent on rotation)
+		int width = metrics.widthPixels;	//get screen width (dependent on rotation)
+		
+		/*Display display = wm.getDefaultDisplay();
+		
+		if (android.os.Build.VERSION.SDK_INT >= 13) {
+			Point size = new Point();
+			display.getSize(size);
+			int width = size.x;
+			int height = size.y;
+		}
+		else {
+			int width = display.getWidth();  // deprecated
+			int height = display.getHeight();  // deprecated
+		}*/
+		
+		Log.i("height, width", height+", "+width);
+		
+		TextPaint text = new TextPaint();
+		text.setTextSize(30);
+		text.setTextAlign(Align.CENTER);
+		text.setColor(0xffffffff);
+		
+		Paint color = new Paint();
+		color.setColor(0xff000000+(int)(Math.random()*0xffffff));
+		
+		for(int i=0; i<10; i++) {
+			buttons.add(new Button(i*width/10, (i+1)*width/10, 4*height/6, 5*height/6, color, i+"", text));
+			color.setColor(0xff000000+(int)(Math.random()*0xffffff));
+		}
 	};
 
 
@@ -76,28 +143,14 @@ public class EmilyView extends SurfaceView implements Runnable,OnTouchListener {
 
 	
 	private void myDraw(Canvas canvas) {
-		Log.i("what is the size of squares?",squares.size()+"");
-		canvas.drawColor(0, Mode.CLEAR);
-		for (int i=0;i<squares.size();i++){
-			Log.i("i",""+i);
-			squares.get(i).draw(canvas);
-			Square square1 = squares.get(i);
-			square1.top = square1.top + 5 + square1.a;
-			square1.bottom = square1.bottom + 5 + square1.a;
-			square1.a++;
-			squares.set(i, square1);
+		for (int i=0;i<buttons.size();i++){
+			buttons.get(i).draw(canvas);
 		}
-		
-		
 	}
 
 	@Override
 	public boolean onTouch(View view, MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_UP){
-			Paint paint = new Paint();
-			paint.setColor((int)(0xff000000+Math.random()*0x00ffffff));
-			squares.add(new Square(event.getX()-10,event.getX()+10,event.getY()-10,event.getY()+10,0,paint));
-		}
+
 		return true;
 	}
 }
