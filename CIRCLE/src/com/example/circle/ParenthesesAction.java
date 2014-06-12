4c41f5746bb489642f501b4d861baf99e331249a
Emily <Emily@Emily-THINK>
2014-06-11 21:07:13 -0500

    jl;jk

---
+++ CIRCLE/src/com/example/circle/ParenthesesAction.java
@@ -0,0 +1,19 @@
+package com.example.circle;
+
+public class ParenthesesAction extends Action {
+	
+	public EmilyView emilyView;
+	
+	public ParenthesesAction(EmilyView emilyView) {
+		super();
+		this.emilyView = emilyView;
+		
+	}
+
+	@Override
+	public void act() {
+		emilyView.selected.parentheses = ! emilyView.selected.parentheses;
+		
+	}
+
+}
